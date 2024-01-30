cba3cd6cf6996e20e84fb6dccd30bb9ed2adbf88
juptofex <julien.damilot@gmail.com>
2024-01-30 11:23:11 +0100

    C DONE

--- Semaine 1/src/VecteurDeCaracteresImpl.java
+++ Semaine 1/src/VecteurDeCaracteresImpl.java
@@ -77,9 +77,11 @@ public class VecteurDeCaracteresImpl implements VecteurDeCaracteres{
 
 	
 	public char element(int rang) throws VecteurOutException {
-		return 0;
 		// TODO 
 		// PENSEZ A CONSULTER LA JAVADOC (cfr Interface VecteurDeCaracteres)
+		if (rang>=taille || rang<0)
+			throw new VecteurOutException();
+		return table[rang];
 	} 
 	
 	public void insere(int rang, char caractere) throws VecteurOutException {
@@ -88,27 +90,62 @@ public class VecteurDeCaracteresImpl implements VecteurDeCaracteres{
 		// il ne peut y avoir des trous
 		// Si la table est pleine, il faut doubler sa capacite
 		// PENSEZ A CONSULTER LA JAVADOC (cfr Interface VecteurDeCaracteres)
+		if (rang>taille || rang<0)
+			throw new VecteurOutException();
+		char[] temp;
+		if (table.length==taille) {
+			temp = new char[table.length*2];
+		} else {
+			temp = new char[table.length];
+		}
+		for (int i = 0; i < rang; i++) {
+			temp[i]=table[i];
+		}
+		temp[rang]=caractere;
+		for (int i = rang+1; i < taille+1; i++) {
+			temp[i]=table[i-1];
+		}
+		table=temp;
+		taille++;
 	}	
 	
 	public void ajoute(char caractere) {
 		// TODO 
 		// Pensez à utiliser la methode insere()!	
 		// PENSEZ A CONSULTER LA JAVADOC (cfr Interface VecteurDeCaracteres)
+		insere(taille, caractere);
 	}
 
 	public char remplace(int rang, char caractere)throws VecteurOutException {
-		return 0;
+
 		// TODO 
 		// PENSEZ A CONSULTER LA JAVADOC (cfr Interface VecteurDeCaracteres)
+		if (rang>=taille || rang<0)
+			throw new VecteurOutException();
+		char temp = table[rang];
+		table[rang]=caractere;
+		return temp;
 	}
 
 	public char supprime(int rang) throws VecteurOutException {
-		return 0;
 		// TODO 
 		// l'ordre des caracteres doit etre conserve --> decalages!!!
 		// il ne peut y avoir des trous
 		// PENSEZ A CONSULTER LA JAVADOC (cfr Interface VecteurDeCaracteres)
+		if (rang>=taille || rang<0)
+			throw new VecteurOutException();
+		char[] temp = new char[table.length];
+		char index = table[rang];
+		for (int i = 0; i < rang; i++) {
+			temp[i]=table[i];
+		}
 
+		for (int i = rang; i < taille-1; i++) {
+			temp[i]=table[i+1];
+		}
+		table=temp;
+		taille--;
+		return index;
 	}
 
 }
