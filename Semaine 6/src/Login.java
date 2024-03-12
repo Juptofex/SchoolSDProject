83ed017c869dbd896391f81a555708e57bc6c9f2
juptofex <julien.damilot@gmail.com>
2024-03-12 09:59:34 +0100

    Week 6 exo 2 DONE

--- Semaine 6/src/Login.java
+++ Semaine 6/src/Login.java
@@ -16,10 +16,16 @@ public class Login {
 	// renvoie un entier >= 0
 	// cet entier represente de façon unique le login courant!
 	public int hashCode(){
-		if (login.substring(0,4).equals("info"))
-			return Integer.parseInt(login.substring(4,7))-1;
-		if (login.substring(0,4).equals("mark"))
-			return Integer.parseInt(login.substring(4,7))+136;
+		if (login.substring(0,4).equals("info")) {
+			if (Integer.parseInt(login.substring(4, 7)) > 137)
+				throw new IllegalArgumentException();
+			return Integer.parseInt(login.substring(4, 7)) - 1;
+		}
+		if (login.substring(0,4).equals("mark")) {
+			if (Integer.parseInt(login.substring(4, 7)) > 73)
+				throw new IllegalArgumentException();
+			return Integer.parseInt(login.substring(4, 7)) + 136;
+		}
 		return Integer.parseInt(login.substring(4,7))+209;
 	}
 	
